18d93a938cc1cc397f6ad64348fcc5cf68f2aba3
Nguyen Le <lenguyen_d@hotmail.com>
2023-12-07 22:40:47 -0800

    Problem 16

--- questions.scm
+++ questions.scm
@@ -22,7 +22,14 @@
 ;; the merged lists.
 (define (merge ordered? s1 s2)
   ; BEGIN PROBLEM 16
-  'replace-this-line
+  (cond ((null? s1) s2)
+        ((null? s2) s1)
+        ((ordered? (car s1) (car s2))
+            (cons (car s1)
+                (merge ordered? (cdr s1) s2)))
+        (else (cons (car s2)
+                (merge ordered? s1 (cdr s2))))
+  )
   )
   ; END PROBLEM 16
 
